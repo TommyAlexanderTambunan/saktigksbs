1ed18272bcb3a5626b6f4e5879bc0a2f7965deab
Tommy Alexander Tambunan <37480141+TommyAlexanderTambunan@users.noreply.github.com>
2026-06-07 19:37:02 +0700

    SW v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sakti-v14-fix2-cache';
+const CACHE='sakti-v14-cache';
 const URLS=['./'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
