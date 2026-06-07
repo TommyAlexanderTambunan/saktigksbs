5aba57a944ea5313d583027ce6d339fca3714762
Tommy Alexander Tambunan <37480141+TommyAlexanderTambunan@users.noreply.github.com>
2026-06-07 19:19:10 +0700

    SW v13

--- sw.js
+++ sw.js
@@ -1,23 +1,6 @@
-const CACHE = 'sakti-v12-logo3-cache';
-const URLS = ['./'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  if (e.request.url.endsWith('.html') || e.request.url.endsWith('/')) {
-    e.respondWith(fetch(e.request).then(r => {
-      const clone = r.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return r;
-    }).catch(() => caches.match(e.request)));
-  } else {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-  }
+const CACHE='sakti-v13-fixlogo-cache';
+const URLS=['./'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.url.endsWith('.html')||e.request.url.endsWith('/')){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));}else{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));}
 });
