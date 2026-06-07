c2858452dbeb051fdf37ba04f0f4b440786fd52b
Tommy Alexander Tambunan <37480141+TommyAlexanderTambunan@users.noreply.github.com>
2026-06-08 04:58:20 +0700

    SW v15

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='sakti-v14-cache';
+const CACHE='sakti-v15-cache';
 const URLS=['./'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{if(e.request.url.endsWith('.html')||e.request.url.endsWith('/')){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));}else{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));}
 });
